959121e3721fbcf23a5233f571561456461ee539
sweta kumari <119477006+Shwetakumari9555@users.noreply.github.com>
2023-03-28 08:10:00 -0700

    Update product.js

--- scripts/product.js
+++ scripts/product.js
@@ -1,9 +1,2 @@
 import { LSK } from "./main.js";
-document.addEventListener("DOMContentLoaded",function(){
-    async function fecthProducts(url){
-        let data= await fetch(url);
-        let response= await data.json();
-        console.log(response)
-    }
-    fecthProducts("https://64214f5434d6cd4ebd6fd51c.mockapi.io/products")
-})
+
