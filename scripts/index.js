6736a5d91843bf4baf41a686fdbfde43298fa628
chiku7296 <ps7291915528@gmail.com>
2023-03-30 20:59:46 +0530

    day-3

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,19 @@
 import { LSK } from "./main.js";
+var log=JSON.parse(localStorage.getItem("logged"))
+var user=JSON.parse(localStorage.getItem("user"))
+var logout=document.getElementById("logout")
+var wlcm=document.getElementById("welcome")
+   for(let i=0;i<user.length;i++){
+    if(log==user[i].id){
+        wlcm.innerHTML=user[i].name
+    }
+   }
+   logout.addEventListener("click",function(){
+       localStorage.setItem("logged",0)
+       alert("Log Out Successfully")
+       setTimeout(function(){
+        location="./index.html"
+       },300)
 
+   })
    
--- scripts/signup.js
+++ scripts/signup.js
@@ -2,25 +2,41 @@ import { LSK } from "./main.js";
 var name=document.getElementById("name")
 var email=document.getElementById("email")
 var phone=document.getElementById("mobile")
-var address=document.getElementById("address")
+
 var conPswrd=document.getElementById("password")
 var rowPswrd=document.getElementById("rowpswrd")
 var continuBtn=document.getElementById("registerBtn")
-
+  var user=JSON.parse(localStorage.getItem("user"))||[]
+  var count=0
+  if(user.length==0){
+    count=0
+  }
+  else{
+    count=user[user.length-1].id
+  }
 continuBtn.addEventListener("click",function(){
-    if(name.value==""||email.value==""||address.value==""||conPswrd.value==""||phone.value==""){
+    if(name.value==""||email.value==""||conPswrd.value==""||phone.value==""){
         alert("Please Enter Valid Details")
     }
     else if(conPswrd.value!==rowPswrd.value){
         alert("Password not matched")
     }
     else{
-        LSK.name=name.value
-        LSK.email=email.value
-        LSK.phone=phone.value
-        LSK.password=conPswrd.value
-        LSK.address=address.value
-        
-        
+        count++
+        let obj={}
+        obj.id=count
+        obj.name=name.value
+        obj.email=email.value
+        obj.phone=phone.value
+        obj.password=conPswrd.value
+        user.push(obj)
+        localStorage.setItem("user",JSON.stringify(user))
+        alert("Registration Successful")
+        setTimeout(function(){
+            location="./signin.html"
+        },500)
+       
     }
 })
+        
+        
