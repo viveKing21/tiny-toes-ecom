b04d388b9f609e6273182ee70c274d761003c06b
Vivek Sharma <vs246246@gmail.com>
2023-04-02 11:15:59 +0530

    Update product_details.js

--- scripts/product_details.js
+++ scripts/product_details.js
@@ -2,6 +2,8 @@ import { LSK, API, currency } from "./main.js";
 
 let query = new URLSearchParams(location.search)
 
+let uid = localStorage.getItem("logged")
+
 if(query.has("pid") == false) {
     alert("Invalid URL")
     location = "/"
@@ -103,12 +105,16 @@ function updateUi(){
             cartBtn.onclick = addToCart
             cartBtn.textContent ="Add to Cart"
         })
-        alert("Product added to cart")
+        alert("Product removed to cart")
     }
     const addToCart = () => {
+        if(uid == null) location = "/signin.html"
+
         let id = carts.reduce((acc, cur) => Math.max(acc, cur.id), 0) + 1
         let cart = {
             pid,
+            uid,
+            qty:1,
             ...productData,
             id
         }
@@ -119,7 +125,7 @@ function updateUi(){
             cartBtn.onclick = removeToCart
             cartBtn.textContent ="Remove to Cart"
         })
-        alert("Product removed to cart")
+        alert("Product added to cart")
     }
 
     addCart.forEach(cartBtn => {
